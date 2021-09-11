41fcb4c9cec998df16f4c3d578dbe96dc313c307
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-09-11 12:54:08 +0530

    add input field on new place screen

--- screens/NewPlace.js
+++ screens/NewPlace.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native'
+import { Colors } from '../constants/Colors'
 
 export default function NewPlace() {
+  const [place, setPlace] = useState('')
+  const submitHandler = () => console.log(place)
   return (
-    <View>
-      <Text>New Place</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.title}>New Place</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter place name"
+          onChangeText={(text) => setPlace(text)}
+        />
+        <Button
+          color={Colors.primaryColor}
+          title="Add Now"
+          onPress={submitHandler}
+        />
+      </View>
+    </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  input: {
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 1,
+    marginBottom: 40,
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 20,
+  },
+})
